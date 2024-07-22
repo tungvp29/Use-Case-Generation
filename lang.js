6a64243d653ee6d1de2995ae9ecdfdcdb3de154f
DESKTOP-9PUMK6K\Administrator <tungvp0209@gmail.com>
2024-07-22 08:28:22 +0700

    Add language function

---
+++ lang.js
@@ -0,0 +1,112 @@
+var translations = {
+    en: {
+        'area-draw-title': 'Draw Area',
+        'area-draw-notice': '(Hold Shift to move around)',
+        'area-render-title': 'Render Area',
+        'area-render-alt': 'Image from canvas, check render to automate transfrom canvas to image',
+        'input-label': 'Input Configuration',
+        'input-file-title': 'Input file data',
+        'input-sheetname-label': 'Sheet name',
+        'input-startcell-label': 'Start cell',
+        'input-startcell-input:title': 'Example: B2 (column B, row 2)',
+        'input-endcell-label': 'End cell',
+        'input-endcell-input:title': 'Example: D (to the last row of column D)',
+        'input-idcol-label': 'Index column',
+        'input-idcol-input:title': 'Index of column in [Start cell:End cell] table: Example: table [B2:DD], index of column B is 0, index of column C is 1',
+        'input-idcol-input:placeholder': 'Index of column in [Start cell:End cell] table. Example: table [B2:DD], index of column B is 0, index of column C is 1',
+        'input-namecol-label': 'Use case text column',
+        'input-namecol-input-title': 'Index of column in [Start cell:End cell] table. Example: table [B2:DD], index of column B is 0, index of column D is 2',
+        'input-namecol-input-placeholder': 'Index of column in [Start cell:End cell] table. Example: table [B2:DD], index of column B is 0, index of column D is 2',
+        'input-from-label': 'From',
+        'input-to-label': 'To',
+        'input-uc-per-pic-label': 'Use case per picture',
+        'input-current-draw-label': 'Current UC in picture',
+        'input-btn-draw': 'Draw',
+        'input-btn-draw-next': 'Draw next',
+        'input-btn-draw-prev': 'Draw prev',
+        'input-btn-render': 'Render',
+        'input-btn-export': 'Export',
+        'uc-title': 'Use Case Size',
+        'uc-width': 'Use-case Width',
+        'uc-height': 'Use-case Height',
+        'uc-distance': 'Distance between 2 UC',
+        'uc-person-distance': 'Distance between UC and person',
+        'uc-shape-selector': 'UC Shape',
+        'uc-shape-selector-rectangle': 'Rectangle',
+        'uc-shape-selector-ellipse': 'Ellipse',
+        'person-shape-selector': 'Person Shape',
+        'person-shape-selector-thin': 'Thin body',
+        'person-shape-selector-bigger': 'Bigger body',
+        'left-right-layout': 'Left-Right Layout',
+        'left-right-layout-selector-left': 'Left side',
+        'left-right-layout-selector-right': 'Right side',
+        'left-right-layout-selector-both': 'Both sides',
+        'render-layout': 'Render Layout',
+        'render-layout-label': 'Only render the objects area in canvas',
+    },
+    vi: {
+        'area-draw-title': 'Khu vực vẽ',
+        'area-draw-notice': '(Nhấn Shift để di chuyển)',
+        'area-render-title': 'Render',
+        'area-render-alt': 'Ảnh từ canvas, chọn vẽ để tự động chuyển đổi canvas thành ảnh',
+        'input-label': 'Cấu hình đầu vào',
+        'input-file-title': 'Dữ liệu tệp đầu vào',
+        'input-sheetname-label': 'Tên sheet',
+        'input-startcell-label': 'Ô bắt đầu',
+        'input-startcell-input:title': 'Ví dụ: B2 (cột B, hàng 2)',
+        'input-endcell-label': 'Ô kết thúc',
+        'input-endcell-input:title': 'Ví dụ: D (đến hàng cuối cùng của cột D)',
+        'input-idcol-label': 'Cột chỉ mục',
+        'input-idcol-input:title': 'Chỉ mục cột trong bảng [Ô bắt đầu: Ô kết thúc]: Ví dụ: bảng [B2: DD], chỉ mục cột B là 0, chỉ mục cột C là 1',
+        'input-idcol-input:placeholder': 'Chỉ mục cột trong bảng [Ô bắt đầu: Ô kết thúc]. Ví dụ: bảng [B2: DD], chỉ mục cột B là 0, chỉ mục cột C là 1',
+        'input-namecol-label': 'Cột nội dung UC',
+        'input-namecol-input-title': 'Chỉ mục cột trong bảng [Ô bắt đầu: Ô kết thúc]. Ví dụ: bảng [B2: DD], chỉ mục cột B là 0, chỉ mục cột D là 2',
+        'input-namecol-input-placeholder': 'Chỉ mục cột trong bảng [Ô bắt đầu: Ô kết thúc]. Ví dụ: bảng [B2: DD], chỉ mục cột B là 0, chỉ mục cột D là 2',
+        'input-from-label': 'Vẽ từ UC',
+        'input-to-label': 'Vẽ đến UC',
+        'input-uc-per-pic-label': 'SL UC trên mỗi hình ảnh',
+        'input-current-draw-label': 'UC hiện tại trong hình ảnh',
+        'input-btn-draw': 'Vẽ',
+        'input-btn-draw-next': 'Vẽ tiếp',
+        'input-btn-draw-prev': 'Vẽ lại trước',
+        'input-btn-render': 'Render ảnh',
+        'input-btn-export': 'Xuất ảnh',
+        'uc-title': 'Kích thước Use Case',
+        'uc-width': 'Chiều rộng UC',
+        'uc-height': 'Chiều cao UC',
+        'uc-distance': 'Khoảng cách giữa 2 UC',
+        'uc-person-distance': 'Khoảng cách giữa UC và người',
+        'uc-shape-selector': 'Hình dạng UC',
+        'uc-shape-selector-rectangle': 'Hình chữ nhật',
+        'uc-shape-selector-ellipse': 'Hình elip',
+        'person-shape-selector': 'Hình dạng người',
+        'person-shape-selector-thin': 'Thân hình mảnh',
+        'person-shape-selector-bigger': 'Thân hình to hơn',
+        'left-right-layout': 'Layout trái-phải',
+        'left-right-layout-selector-left': 'Bên trái',
+        'left-right-layout-selector-right': 'Bên phải',
+        'left-right-layout-selector-both': 'Cả hai bên',
+        'render-layout': 'Render Layout',
+        'render-layout-label': 'Chỉ render khu vực các đối tượng trong canvas',
+    }
+}
+
+function changeLanguage(lang) {
+    var elements = document.querySelectorAll('[data-translate]');
+
+    elements.forEach(function(element) {
+        var key = element.getAttribute('data-translate');
+        if (element.placeholder !== undefined) {
+            element.placeholder = translations[lang][key];
+        } else if (element.alt !== undefined) {
+            element.alt = translations[lang][key];
+        } else if (element.title !== undefined) {
+            element.title = translations[lang][key];
+        } 
+        element.textContent = translations[lang][key];
+        
+    });
+}
+
+// Use it like this:
+changeLanguage('vi');
